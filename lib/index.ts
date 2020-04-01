322d1a6cfd371da1a3add03bf44be0a11f60c621
Ismael <ispira90@gmail.com>
2020-04-01 19:42:23 +0200

    fix: detect when running in ts node

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,2 @@
-import { isTsNode } from './utils/isTsNode';
-
 export { MigrationInterface } from './MigrationInterface';
 export { cli as mongoMigrateCli } from './cli';
-
-if (isTsNode()) {
-  console.log('is ts node');
-} else {
-  console.log('NOT ts node');
-}
--- lib/utils/isTsNode.ts
+++ lib/utils/isTsNode.ts
@@ -1,16 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export const isTsNode = (): boolean => {
-  const path = process.env._;
-  if (!path) {
-    return false;
-  }
+  const symbol = Symbol.for('ts-node.register.instance');
 
-  const parts = path.split('/');
-
-  if (parts.length === 0) {
-    return false;
-  }
-
-  const regex = new RegExp('(ts-node|tsnd)');
-
-  return regex.test(parts[parts.length - 1]);
+  return (process as any)[symbol] !== undefined;
 };
