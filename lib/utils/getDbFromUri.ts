d3b620b94362f02cc5229705d5481eb6538d5d2c
Alex Xela <nosuchip@gmail.com>
2021-11-24 18:15:32 +0700

    Add conection-string to parse mongodb URIs

--- lib/utils/getDbFromUri.ts
+++ lib/utils/getDbFromUri.ts
@@ -1,13 +1,13 @@
-import { URL } from 'url';
+import { ConnectionString } from 'connection-string';
 
 export const getDbFromUri = (uri: string): string | undefined => {
-  const parseUri = new URL(uri);
+  const parseUri = new ConnectionString(uri);
 
-  if (!parseUri.pathname || parseUri.pathname.length < 2) {
+  if (!parseUri.path || !parseUri.path.length) {
     return;
   }
 
-  const db = parseUri.pathname.slice(1);
+  const db = parseUri.path[0];
 
   return db;
 };
