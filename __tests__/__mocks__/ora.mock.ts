9d1543bcc163bd22f1e6376da92e4d40e895bbce
Isma <ispira90@gmail.com>
2022-02-05 21:22:17 +0100

    fix oramock in tests

--- __tests__/__mocks__/ora.mock.ts
+++ __tests__/__mocks__/ora.mock.ts
@@ -1,6 +1,7 @@
 export const oraMock = () => ({
   start: () => ({
     succeed: () => ({ stop: () => jest.fn() }),
-    warn: () => ({ stop: () => jest.fn() })
-  })
+    warn: () => ({ stop: () => jest.fn() }),
+    fail: jest.fn(),
+  }),
 });
