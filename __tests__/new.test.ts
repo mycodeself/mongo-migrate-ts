b701c9e8e04a4469ee0be48aaf35ca6e8c79906a
Kristi Achilleos <Krispy2009@gmail.com>
2022-03-29 10:43:19 +0300

    feat(commands): add ability to pass template file to `new` command

--- __tests__/new.test.ts
+++ __tests__/new.test.ts
@@ -2,12 +2,19 @@ jest.mock('fs');
 jest.mock('../lib/config');
 
 import * as fs from 'fs';
-import { newCommand } from '../lib/commands/new';
+import * as newModule from '../lib/commands/new';
 import { configMock } from './__mocks__/config.mock';
 
 describe('new command', () => {
   const mkdirSyncSpy = jest.spyOn(fs, 'mkdirSync');
   const writeFileSyncSpy = jest.spyOn(fs, 'writeFileSync');
+  const defaultTemplateSpy = jest.spyOn(newModule, 'defaultMigrationTemplate');
+
+  beforeAll(() => {
+    jest
+      .useFakeTimers()
+      .setSystemTime(new Date('2022-01-01 00:00:00.000').getTime());
+  });
 
   beforeEach(() => {
     mkdirSyncSpy.mockReset();
@@ -17,7 +24,10 @@ describe('new command', () => {
   it('should create a new migration file', () => {
     (fs.existsSync as jest.Mock).mockReturnValue(true);
     const migrationName = 'TestMigration';
-    newCommand({ migrationName, migrationsDir: configMock.migrationsDir });
+    newModule.newCommand({
+      migrationName,
+      migrationsDir: configMock.migrationsDir,
+    });
 
     expect(writeFileSyncSpy).toHaveBeenCalled();
   });
@@ -26,9 +36,56 @@ describe('new command', () => {
     (fs.existsSync as jest.Mock).mockReturnValue(false);
 
     const migrationName = 'TestMigration';
-    newCommand({ migrationName, migrationsDir: configMock.migrationsDir });
+    newModule.newCommand({
+      migrationName,
+      migrationsDir: configMock.migrationsDir,
+    });
 
     expect(mkdirSyncSpy).toHaveBeenCalledWith(configMock.migrationsDir);
     expect(writeFileSyncSpy).toHaveBeenCalled();
+  });
+
+  it('should create the migration using a template if the provided file exists', () => {
+    const templateFileText = 'custom template file contents';
+
+    (fs.existsSync as jest.Mock).mockReturnValue(true);
+    (fs.readFileSync as jest.Mock).mockReturnValue(templateFileText);
+
+    const templateFile = 'testFile.ts';
+    newModule.newCommand({
+      migrationsDir: configMock.migrationsDir,
+      templateFile: templateFile,
+    });
+    const fileName = `${+new Date()}_Migration`;
+    const expectedMigrationsPath = `${configMock.migrationsDir}/${fileName}.ts`;
+
+    expect(writeFileSyncSpy).toHaveBeenCalledWith(
+      expectedMigrationsPath,
+      templateFileText
+    );
+  });
+
+  it('should use the default template text if template file does not exist', () => {
+    const defaultTemplateText = 'default template file contents';
+
+    (fs.existsSync as jest.Mock).mockReturnValue(false);
+    (newModule.defaultMigrationTemplate as jest.Mock).mockReturnValue(
+      defaultTemplateText
+    );
+
+    newModule.newCommand({
+      migrationsDir: configMock.migrationsDir,
+      templateFile: 'doesNotExist.ts',
+    });
+
+    const fileName = `${+new Date()}_Migration`;
+
+    const expectedMigrationsPath = `${configMock.migrationsDir}/${fileName}.ts`;
+
+    expect(defaultTemplateSpy).toHaveBeenCalled();
+    expect(writeFileSyncSpy).toHaveBeenCalledWith(
+      expectedMigrationsPath,
+      defaultTemplateText
+    );
   });
 });
--- lib/commands/new.ts
+++ lib/commands/new.ts
@@ -3,9 +3,10 @@ import * as fs from 'fs';
 interface CommandNewOptions {
   migrationsDir: string;
   migrationName?: string;
+  templateFile?: string;
 }
 
-export const migrationTemplate = (className: string) => {
+export const defaultMigrationTemplate = (className: string) => {
   return `import { Db } from 'mongodb'
 import { MigrationInterface } from 'mongo-migrate-ts';
 
@@ -20,15 +21,21 @@ export class ${className} implements MigrationInterface {
 };
 
 export const newCommand = (opts: CommandNewOptions): string => {
-  const { migrationName, migrationsDir } = opts;
+  const { migrationName, migrationsDir, templateFile } = opts;
 
   if (!fs.existsSync(migrationsDir)) {
     fs.mkdirSync(migrationsDir);
   }
-
+  let template: string;
   const fileName = `${+new Date()}_${migrationName || 'Migration'}`;
   const className = `${migrationName || 'Migration'}${+new Date()}`;
-  const template = migrationTemplate(className);
+  console.log(templateFile);
+
+  if (templateFile && fs.existsSync(templateFile)) {
+    template = fs.readFileSync(templateFile).toString();
+  } else {
+    template = defaultMigrationTemplate(className);
+  }
   const migrationPath = `${migrationsDir}/${fileName}.ts`;
 
   fs.writeFileSync(migrationPath, template);
