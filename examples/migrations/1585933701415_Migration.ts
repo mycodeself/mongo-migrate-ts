b24ee5407b57bcacdd4494e0daa6a29ad1de9c95
Isma <ispira90@gmail.com>
2023-03-11 01:56:07 +0100

    test fail migration ci

--- examples/migrations/1585933701415_Migration.ts
+++ examples/migrations/1585933701415_Migration.ts
@@ -4,6 +4,7 @@ import { MigrationInterface } from '../../lib';
 export class Migration1585933701415 implements MigrationInterface {
   public async up(db: Db): Promise<any> {
     db.createCollection('mycol');
+    db.createCollection('mycol');
   }
 
   public async down(db: Db): Promise<any> {
