53e5b42332373f7dbd658485298e7af0ecd0e9a8
Isma <ispira90@gmail.com>
2023-03-11 02:02:56 +0100

    revert throw test error

--- examples/migrations/1585933701415_Migration.ts
+++ examples/migrations/1585933701415_Migration.ts
@@ -4,7 +4,6 @@ import { MigrationInterface } from '../../lib';
 export class Migration1585933701415 implements MigrationInterface {
   public async up(db: Db): Promise<any> {
     db.createCollection('mycol');
-    throw new Error('Test error');
   }
 
   public async down(db: Db): Promise<any> {
