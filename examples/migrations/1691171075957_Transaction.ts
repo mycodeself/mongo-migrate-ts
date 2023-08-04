93b8eb9e1150c53b715621660a90c3cf6752e854
Isma <ispira90@gmail.com>
2023-08-04 19:51:56 +0200

    pass mongo client to migration to allow create sessions

---
+++ examples/migrations/1691171075957_Transaction.ts
@@ -0,0 +1,30 @@
+import { Db, MongoClient } from 'mongodb';
+import { MigrationInterface } from '../../lib';
+
+export class Transaction1691171075957 implements MigrationInterface {
+  public async up(db: Db, client: MongoClient): Promise<any> {
+    const session = client.startSession();
+    try {
+      await session.withTransaction(async () => {
+        await db.collection('mycol').insertOne({ foo: 'one' });
+        await db.collection('mycol').insertOne({ foo: 'two' });
+        await db.collection('mycol').insertOne({ foo: 'three' });
+      });
+    } finally {
+      await session.endSession();
+    }
+  }
+
+  public async down(db: Db, client: MongoClient): Promise<any> {
+    const session = client.startSession();
+    try {
+      await session.withTransaction(async () => {
+        await db.collection('mycol').deleteOne({ foo: 'one' });
+        await db.collection('mycol').deleteOne({ foo: 'two' });
+        await db.collection('mycol').deleteOne({ foo: 'three' });
+      });
+    } finally {
+      await session.endSession();
+    }
+  }
+}
