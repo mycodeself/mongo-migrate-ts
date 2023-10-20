c6b506de154f0daf4559be25578dbf5e40511367
Aleksei Strizhak <alexei.mifrill.strizhak@gmail.com>
2023-10-20 08:50:13 +0100

    feat: unify arguments for examples

--- examples/migrations/1585933701415_Migration.ts
+++ examples/migrations/1585933701415_Migration.ts
@@ -1,12 +1,12 @@
-import { Db } from 'mongodb';
+import { Db, MongoClient } from 'mongodb';
 import { MigrationInterface } from '../../lib';
 
 export class Migration1585933701415 implements MigrationInterface {
-  public async up(db: Db): Promise<void> {
-    db.createCollection('mycol');
+  public async up(db: Db, _client: MongoClient): Promise<void | never> {
+    await db.createCollection('mycol');
   }
 
-  public async down(db: Db): Promise<void> {
-    db.dropCollection('mycol');
+  public async down(db: Db, _client: MongoClient): Promise<void | never> {
+    await db.dropCollection('mycol');
   }
 }
