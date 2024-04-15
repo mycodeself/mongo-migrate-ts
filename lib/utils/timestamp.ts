0dc0f02bc4e8c105950d3598a5f081e2313b68d5
Aleksei Strizhak <alexei.mifrill.strizhak@gmail.com>
2024-04-15 19:57:15 +0100

    chore: convert Timestamp class to function

--- lib/utils/timestamp.ts
+++ lib/utils/timestamp.ts
@@ -1,21 +1,16 @@
 import { format } from 'date-fns';
 
-export class Timestamp {
-  constructor(private readonly date = new Date()) {}
-
-  public toString(dateFormat: string): string {
-    return format(this.#date(), dateFormat);
-  }
-
-  #date(): Date {
-    return new Date(
-      this.date.getUTCFullYear(),
-      this.date.getUTCMonth(),
-      this.date.getUTCDate(),
-      this.date.getUTCHours(),
-      this.date.getUTCMinutes(),
-      this.date.getUTCSeconds(),
-      this.date.getUTCMilliseconds()
-    );
-  }
-}
+export const timestamp = (dateFormat: string, date = new Date()): string => {
+  return format(
+    new Date(
+      date.getUTCFullYear(),
+      date.getUTCMonth(),
+      date.getUTCDate(),
+      date.getUTCHours(),
+      date.getUTCMinutes(),
+      date.getUTCSeconds(),
+      date.getUTCMilliseconds()
+    ),
+    dateFormat
+  );
+};
