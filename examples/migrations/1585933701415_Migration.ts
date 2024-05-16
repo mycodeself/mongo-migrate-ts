6455efb3bd01e0e7cf7f62d8a076a7883e63c449
Aleksei Strizhak <alexei.mifrill.strizhak@gmail.com>
2024-05-16 22:57:43 +0100

    chore: remove 'client' reference in default template

--- examples/migrations/1585933701415_Migration.ts
+++ examples/migrations/1585933701415_Migration.ts
@@ -1,12 +1,12 @@
-import { Db, MongoClient } from 'mongodb';
+import { Db } from 'mongodb';
 import { MigrationInterface } from '../../lib';
 
 export class Migration1585933701415 implements MigrationInterface {
-  public async up(db: Db, _client: MongoClient): Promise<void | never> {
+  public async up(db: Db): Promise<void | never> {
     await db.createCollection('mycol');
   }
 
-  public async down(db: Db, _client: MongoClient): Promise<void | never> {
+  public async down(db: Db): Promise<void | never> {
     await db.dropCollection('mycol');
   }
 }
